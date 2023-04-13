c9903eb7930c568b240d433e14bd31c545128526
ehssamimi <ehssamimi@yahoo.com>
2023-04-13 20:21:55 +0430

    Updates

---
+++ precache-manifest.0ada6a4bf7a9d580334a83efc957e9cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bde52ee25fc80836351",
+    "url": "/pdf-aanotate/static/css/main.c72e0122.chunk.css"
+  },
+  {
+    "revision": "8bde52ee25fc80836351",
+    "url": "/pdf-aanotate/static/js/main.8bde52ee.chunk.js"
+  },
+  {
+    "revision": "2f1469cbf198d5064ade",
+    "url": "/pdf-aanotate/static/js/1.2f1469cb.chunk.js"
+  },
+  {
+    "revision": "74a9c0b5cd5c4a7d7905",
+    "url": "/pdf-aanotate/static/js/runtime~main.74a9c0b5.js"
+  },
+  {
+    "revision": "2af9b304963ab5993378d2c119e0fe79",
+    "url": "/pdf-aanotate/index.html"
+  }
+];
